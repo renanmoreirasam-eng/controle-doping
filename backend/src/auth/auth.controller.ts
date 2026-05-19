a6db8d1209ea204b41d0e42a795e7b12419d78ad
Renan Sampaio <renanmoreirasam@gmail.com>
2026-05-19 11:45:40 -0300

    v0.3.0 - registro de usuários com bcrypt

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('register')
+  async register(
+    @Body()
+    body: {
+      name: string;
+      email: string;
+      password: string;
+    },
+  ) {
+    return this.authService.register(body);
+  }
+}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async register(data: {
+    name: string;
+    email: string;
+    password: string;
+  }) {
+    const userExists = await this.usersService.findByEmail(data.email);
+
+    if (userExists) {
+      throw new UnauthorizedException('Usuário já existe');
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    const user = await this.usersService.create({
+      ...data,
+      password: hashedPassword,
+    });
+
+    return user;
+  }
+}
